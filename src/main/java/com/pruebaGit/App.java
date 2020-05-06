17518c991d9d68e9b262a432945f619733ab2ba6
Francisco <1830275@upv.edu.mx>
2020-05-05 23:19:41 -0500

    Agregacion de la clase Persona

--- src/main/java/com/pruebaGit/App.java
+++ src/main/java/com/pruebaGit/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+        Persona persona = new Persona();
         System.out.println( "Hello World!" );
+
+        persona.mensaje();
+
     }
 }
---
+++ src/main/java/com/pruebaGit/Persona.java
@@ -0,0 +1,16 @@
+package com.pruebaGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class Persona {
+
+
+
+    public void mensaje(){
+        System.out.printf("Mensaje de hola");
+    }
+   
+    
+}
