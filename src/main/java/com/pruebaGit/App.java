61e938757b2e44cea1854e763507ba6c0752ee05
Alan Zapata <1830141@upv.edu.mx>
2020-05-06 00:14:33 -0500

    sume dos numeritos uwu

--- src/main/java/com/pruebaGit/App.java
+++ src/main/java/com/pruebaGit/App.java
@@ -8,10 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        Persona persona = new Persona();
+    ;
+        Persona respuesta = new Persona(2,2);
         System.out.println( "Hello World!" );
 
-        persona.mensaje();
+        respuesta.mensaje();
+
+        System.out.println(respuesta.suma());
 
     }
 }
--- src/main/java/com/pruebaGit/Persona.java
+++ src/main/java/com/pruebaGit/Persona.java
@@ -6,11 +6,21 @@ package com.pruebaGit;
  */
 public class Persona {
 
+    private int a,b, respuesta;
 
+    public Persona(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
 
     public void mensaje(){
         System.out.printf("Mensaje de hola");
     }
    
-    
+    public int suma(){
+
+        respuesta=a+b;
+        return respuesta;
+    }
 }
+
