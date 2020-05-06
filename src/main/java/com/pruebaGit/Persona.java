5ff09f20fe603db11015fa787aefe040a0c7ef16
Loperena <1830472@upv.edu.mx>
2020-05-06 01:43:49 -0500

    Con burbuja

--- src/main/java/com/pruebaGit/Persona.java
+++ src/main/java/com/pruebaGit/Persona.java
@@ -8,11 +8,11 @@ import java.util.ArrayList;
  */
 public class Persona {
 
-<<<<<<< HEAD
-    private int a,b, respuesta;
-=======
-    public ArrayList lista = new ArrayList();
 
+    private int a,b, respuesta;
+
+    public ArrayList lista = new ArrayList();
+    Burbuja burbuja = new Burbuja();
     public void arrayedit(int a){
         for (int i=0;i<5;i++){
             lista.add(i,34+a);
@@ -20,9 +20,20 @@ public class Persona {
         for (int i=0;i<5;i++){
             System.out.println(lista.get(i));
         }
+        this.aBurbuja(); //Aqui llamará el método "aBurbuja" de su propia clase. Es la que está abajo.
+    }
+    public void aBurbuja(){
+        //Agregué valores random a la lista
+         ArrayList lista = new ArrayList();
+         lista.add(0,20);
+         lista.add(0,37);
+         lista.add(0,7);
+         lista.add(0,52);
+         lista.add(0,47);
+         //Se manda esta lista al array que está en la clase Burbuja
+        burbuja.setArray(lista);
     }
 
->>>>>>> 30b8942b949afc9f3cc61602c4367ddf3b81e972
 
     public Persona(int a, int b){
         this.a=a;
